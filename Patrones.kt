75d52c67f50e491d4647954b536c3a9ffd10d5c1
Maria Belen Becerra <belustianabecerra@gmail.com>
2025-11-15 12:40:51 -0400

    State
    Update del patron State

--- Patrones.kt
+++ Patrones.kt
@@ -12,6 +12,52 @@ object ReminderManager {
     }
 }
 
+interface MedicationState {
+    fun take()
+    fun skip()
+}
+class Medication(var name: String) {
+    var state: MedicationState = PendingState(this)
+
+    fun take() = state.take()
+    fun skip() = state.skip()
+}
+
+class PendingState(private val med: Medication) : MedicationState {
+    override fun take() {
+        println("${med.name}: marcado como tomado")
+        med.state = TakenState(med)
+    }
+
+    override fun skip() {
+        println("${med.name}: marcado como omitido ⚠")
+        med.state = SkippedState(med)
+    }
+}
+
+class TakenState(private val med: Medication) : MedicationState {
+    override fun take() {
+        println("${med.name}: ya fue tomado")
+    }
+
+    override fun skip() {
+        println("${med.name}: no se puede omitir, ya fue tomado")
+    }
+}
+
+class SkippedState(private val med: Medication) : MedicationState {
+    override fun take() {
+        println("${med.name}: cambiado a tomado (recuperado)")
+        med.state = TakenState(med)
+    }
+
+    override fun skip() {
+        println("${med.name}: ya fue omitido")
+    }
+}
+
+
+
 
 fun main() {
    println("=== PATRONDE DISEÑO ===\n")
@@ -20,5 +66,15 @@ fun main() {
     ReminderManager.addReminder("Tomar Paracetamol a las 8:00")
     ReminderManager.addReminder("Tomar Ibuprofeno a las 14:00")
     ReminderManager.showReminders()
+    
+    println("\n PATRÓN STATE")
+    val med = Medication("Paracetamol")
+    med.take()
+    med.skip()
+    println("--- Reiniciando otro medicamento ---")
+    val med2 = Medication("Ibuprofeno")
+    med2.skip()
+    med2.take()
+
 
 }
